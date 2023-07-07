6bdc100dd9d51d3a4f989bb1a6f990c3741a115d
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2023-07-07 22:50:11 +0100

    chore: updated api response to make it more readable and upgrade ruby version.

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -4,37 +4,37 @@ class BusinessController < ApplicationController
     def create_business
         business_obj = CreateBusinessService.call(params[:title], @current_user.id)
         if business_obj.valid?
-            api_response true, "Business successfully created", business_obj, :created
+            api_response(status: true, message: "Business successfully created", data: business_obj, status: :created)
         else
-            api_response false, business_obj.errors.objects.first.full_message, nil, :unprocessable_entity
+            api_response(status: false, message: business_obj.errors.objects.first.full_message, data: nil, status: :unprocessable_entity)
         end
     end
 
     def get_all_business
         businesses_obj = GetAllBusinessService.call(@current_user)
-        api_response true, "Business successfully fetched", businesses_obj, :ok
+        api_response(status: true, message: "Business successfully fetched", data: businesses_obj, status: :ok)
     end
 
     def edit_business
         business_obj = EditBusinessService.call({slug: params[:slug]}, business_params)
-        api_response true, "Business successfully updated", business_obj, :ok
+        api_response(status: true, message: "Business successfully updated", data: business_obj, status: :ok)
     end
 
     def api_keys
         business_obj = GetBusinessService.call({slug: params[:slug]})
         if business_obj.present?
-            api_response true, "Keys successfully fetched", business_obj.api_key, :ok
+            api_response(status: true, message: "Keys successfully fetched", data: business_obj.api_key, status: :ok)
         else
-            api_response false, "Keys does not exit", nil, :not_found
+            api_response(status: false, message: "Keys does not exit", data: nil, status: :not_found)
         end
     end
 
     def generate_api_keys
         api_key_obj = GenerateApiKeyService.call(params[:business_slug])
         if api_key_obj.present?
-            api_response true, "Keys successfully generated", api_key_obj, :ok
+            api_response(status: true, message: "Keys successfully generated", data: api_key_obj, status: :ok)
         else
-            api_response false, "Could not generate keys does not exit", nil, :not_found
+            api_response(status: false, message: "Could not generate keys does not exit", data: nil, status: :not_found)
         end
     end
 
