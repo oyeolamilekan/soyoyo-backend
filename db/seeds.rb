71eb4414eb2923b6decd7964d9bf57c304bc48bf
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2023-07-10 10:45:19 +0100

    feat: added seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env == 'development'
+    PASSWORD = 'password'
+    # Create some users
+    User.create(first_name: 'Adigun', last_name: 'Efon', email: 'adigun@appstate.co', password: PASSWORD)
+    User.create(first_name: 'Adigun', last_name: 'Efon', email: 'adigun2@appstate.co', password: PASSWORD)
+
+    # Create Business
+    Business.create(title: 'Oye Enterprise', user_id: User.find_by(email: 'adigun@appstate.co').id)
+    Business.create(title: 'Happy state Enterprise', user_id: User.find_by(email: 'adigun@appstate.co').id)
+    Business.create(title: 'Lakeside Enterprise', user_id: User.find_by(email: 'adigun2@appstate.co').id)
+    Business.create(title: 'Oye Housing', user_id: User.find_by(email: 'adigun2@appstate.co').id)
+
+    # Create Payment Pages
+    PaymentPage.create(title: 'Car payment', business_id: Business.find_by(title: 'Oye Enterprise').id)
+    PaymentPage.create(title: 'Simple Donation', business_id: Business.find_by(title: 'Happy state Enterprise').id)
+    PaymentPage.create(title: 'Car Savings', business_id: Business.find_by(title: 'Lakeside Enterprise').id)
+end
