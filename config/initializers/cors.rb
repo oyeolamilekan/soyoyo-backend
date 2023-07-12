cdcb2a1e299fc8ba27607774c8d3d6630cbeae53
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2023-07-13 00:09:14 +0100

    chore: some fix pushed.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Set your allowed origins here or specify specific domains
+    origins "*"
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
